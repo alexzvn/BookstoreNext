bf648a4ab48eb5052d5872fcaa99ec460e19ed62
Alex <41188285+alexzvn@users.noreply.github.com>
2022-12-16 21:57:50 +0700

    init stage

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookstoreNext.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
